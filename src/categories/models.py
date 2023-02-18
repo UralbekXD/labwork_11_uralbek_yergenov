11baf37b67340766146dec287beca5a6d2736967
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-19 02:45:53 +0600

    Create 'category' model

--- src/categories/models.py
+++ src/categories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False, verbose_name='Наименование')
+    description = models.TextField(max_length=2048, null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
