ca5222e191e859c57f9693720cad84b2199f8a54
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-19 18:45:48 +0600

    Add product_view and add in urls

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import products_view
+from products.views import products_view, product_view
 
 urlpatterns = [
     path('', products_view),
     path('products/', products_view),
+    path('products/<int:pk>', product_view),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -9,3 +9,17 @@ def products_view(request):
     return render(request, 'products/index.html', context={
         'products': products,
     })
+
+
+def product_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'created_at': product.created_at.strftime('%d/%m/%Y %H:%M'),
+        'category': product.category.name,
+        'image': product.image,
+    }
+
+    return render(request, 'products/product.html', context=context)
