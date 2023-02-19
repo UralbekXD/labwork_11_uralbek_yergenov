a67dc1517bda3d50cdb8212ef4566468a7afea16
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-19 16:49:15 +0600

    Add 'products_view'

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from products.models import Product
+
 
 def products_view(request):
-    pass
+    products = Product.objects.all()
+
+    return render(request, 'products/index.html', context={
+        'products': products,
+    })
