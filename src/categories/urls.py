2c0d512393d6adea6565dbf0d5c8d2e20050ba91
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-19 20:54:46 +0600

    Add names to categories urls

--- src/categories/urls.py
+++ src/categories/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from categories.views import categories_view, category_add_view
 
 urlpatterns = [
-    path('', categories_view),
-    path('add/', category_add_view),
+    path('', categories_view, name='categories_view'),
+    path('add/', category_add_view, name='category_add_view'),
 ]
