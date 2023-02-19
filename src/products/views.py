453f8ee7e560b088c889b1ecb984bd7592f229cb
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-19 20:30:15 +0600

    Add product_view to see more about certain product

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from products.models import Product
+from categories.models import Category
 
 
 def products_view(request):
@@ -23,3 +25,26 @@ def product_view(request, pk):
     }
 
     return render(request, 'products/product.html', context=context)
+
+
+def product_add_view(request):
+    match request.method:
+        case 'GET':
+            categories = Category.objects.all()
+            context = {
+                'categories': categories,
+            }
+            return render(request, 'products/product_add.html', context=context)
+
+        case 'POST':
+            category_id = request.POST.get('category')
+            category = Category.objects.get(pk=category_id)
+            product = Product.objects.create(
+                name=request.POST.get('name'),
+                description=request.POST.get('description'),
+                price=request.POST.get('price'),
+                image=request.POST.get('image'),
+                category=category,
+            )
+
+            return redirect(reverse('product_view', kwargs={'pk': product.pk}))
