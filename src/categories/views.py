e78c305f882bc0a7c721bba311dadce18a066c7c
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-20 14:33:16 +0600

    Add categories and category_add views with following templates

--- src/categories/views.py
+++ src/categories/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from categories.models import Category
 
 
 def categories_view(request):
-    pass
+    categories = Category.objects.all()
+
+    return render(request, 'categories/index.html', context={
+        'categories': categories,
+    })
 
 
 def category_add_view(request):
-    pass
+    match request.method:
+        case 'GET':
+            return render(request, 'categories/category_add.html')
+
+        case 'POST':
+            Category.objects.create(
+                name=request.POST.get('name'),
+                description=request.POST.get('description'),
+            )
+
+            return redirect('categories_view')
