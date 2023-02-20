053c965d2e913e935acc19c0eef11a5243f4c060
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-20 15:31:25 +0600

    Add category edit view and configure in urls, fill form with category data

--- src/categories/urls.py
+++ src/categories/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from categories.views import categories_view, category_add_view
+from categories.views import categories_view, category_add_view, category_edit_view
 
 urlpatterns = [
     path('', categories_view, name='categories_view'),
     path('add/', category_add_view, name='category_add_view'),
+    path('<int:pk>/edit/', category_edit_view, name='category_edit_view'),
 ]
--- src/categories/views.py
+++ src/categories/views.py
@@ -23,3 +23,20 @@ def category_add_view(request):
             )
 
             return redirect('categories_view')
+
+
+def category_edit_view(request, pk):
+    match request.method:
+        case 'GET':
+            category = Category.objects.get(pk=pk)
+            return render(request, 'categories/category_edit.html', context={
+                'category': category,
+            })
+
+        case 'POST':
+            category = Category.objects.get(pk=pk)
+            category.name = request.POST.get('name')
+            category.description = request.POST.get('description')
+            category.save()
+
+            return redirect('categories_view')
